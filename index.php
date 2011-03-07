213a44a0a2f41a334d4c8bdbedf2f48fcd5876c6
seek <seekshiva@gmail.com>
2011-03-07 15:01:25 +0530

    The initial stage... just created the imitation of basic styles of emacs
    Committer: seek <seekshiva@gmail.com>

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<title>Emacs</title>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div id="menubar">
+File Edit Options Buffers Tools Help
+</div>
+<div class="bufferC">
+<div class="buffer">hi</div>
+<div class="statusbar">
+-111:**--F1&nbsp;&nbsp;&nbsp;index.php&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All L14&nbsp;&nbsp;&nbsp;&nbsp;(Fundamental)---------------------------------------------------------------------------------------------------------------------
+</div>
+</div>
+<div id="minibuffer">
+C-x C-g is undefined
+</div>
+</body>
+</html>
